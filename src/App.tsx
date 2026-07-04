bf934b227ec0011c4c7ae5c082fcedeeba2cb246
Alexandre-Mir <xandenextel@gmail.com>
2026-07-04 10:33:03 -0300

    feat: convert css modules styling to styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
+import { ThemeProvider } from 'styled-components'
+
 import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
 
-import './global.css'
+import EstiloGlobal, { Container } from './global'
+import temaPadrao from './themes'
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={temaPadrao}>
+      <EstiloGlobal />
       <Header />
       <Hero />
-      <div className="container">
+      <Container>
         <ListaVagas />
-      </div>
-    </>
+      </Container>
+    </ThemeProvider>
   )
 }
 
